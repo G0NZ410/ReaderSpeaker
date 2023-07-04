3c2d6a9e4e86ccbfaa868a85ad14ddac924974ac
G0NZ410 <sandoval.vasquez.gon@gmail.com>
2023-07-03 21:23:30 -0600

    finished

--- rsmain.py
+++ rsmain.py
@@ -2,12 +2,12 @@ import time
 import pyperclip
 import pyttsx3
 
-toSpeak = pyttsx3.init()
-
 def subrayar_texto():
     time.sleep(2)
     texto_copiado = pyperclip.paste()
     return texto_copiado
 
+toSpeak = pyttsx3.init()
+toSpeak.setProperty('rate', 125)  # Ajusta la velocidad del habla a 0.8 (más lento)
 toSpeak.say(subrayar_texto())
 toSpeak.runAndWait()
